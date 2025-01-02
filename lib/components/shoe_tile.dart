91bbaad299dd78797234a190ad0d5ddbc5014584
Calculator App <ayanhussain841@gmail.com>
2025-01-02 19:06:44 +0530

    first

--- lib/components/shoe_tile.dart
+++ lib/components/shoe_tile.dart
@@ -3,38 +3,56 @@ import 'package:shoeapp/Models/shoe.dart';
 import 'package:shoeapp/pages/item_page.dart';
 
 class ShoeTile extends StatelessWidget {
-  Shoe shoe;
-  void Function()? onTap;
-   ShoeTile({super.key, required this.shoe, required this.onTap});
+  final Shoe shoe;
+  final void Function()? onTap;
+
+  ShoeTile({super.key, required this.shoe, required this.onTap});
+
   @override
   Widget build(BuildContext context) {
+    // Getting screen width and height
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
     return Container(
       padding: const EdgeInsets.all(12),
       margin: const EdgeInsets.all(20),
-      width: 300,
+      width: screenWidth * 0.8, // Set width to 80% of screen width
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(18),
-        color: Colors.grey[300]
+        color: Colors.grey[300],
       ),
       child: GestureDetector(
-        onTap: (){
+        onTap: () {
           Navigator.push(
             context,
             MaterialPageRoute(
               builder: (context) => ItemPage(),
               settings: RouteSettings(
-                arguments: shoe,  // Passing the entire Person object
+                arguments: shoe, // Passing the entire Shoe object
               ),
             ),
           );
         },
         child: Column(
           children: [
-            //image
-            Image.asset(shoe.imagePath),
-            //decription
-            Text(shoe.Description),
+            // Image with responsive size
+            Image.asset(
+              shoe.imagePath,
+              width: screenWidth * 0.6, // Image width adjusted to screen width
+              height: screenHeight * 0.2, // Image height based on screen height
+              fit: BoxFit.contain,
+            ),
+
+            // Description with flexible space
+            Text(
+              shoe.Description,
+              style: TextStyle(
+                fontSize: screenWidth * 0.04, // Dynamic font size based on screen width
+              ),
+            ),
             const Spacer(),
+
             Padding(
               padding: const EdgeInsets.only(left: 24),
               child: Row(
@@ -44,13 +62,21 @@ class ShoeTile extends StatelessWidget {
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text(shoe.name,style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 22
+                      // Responsive Name and Price
+                      Text(
+                        shoe.name,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: screenWidth * 0.05, // Dynamic font size
+                        ),
                       ),
+                      const SizedBox(height: 2),
+                      Text(
+                        '\$${shoe.price}',
+                        style: TextStyle(
+                          fontSize: screenWidth * 0.04, // Dynamic font size
+                        ),
                       ),
-                      const SizedBox(height: 2,),
-                      Text('\$' + shoe.price),
                     ],
                   ),
                   GestureDetector(
@@ -60,12 +86,16 @@ class ShoeTile extends StatelessWidget {
                       decoration: const BoxDecoration(
                         color: Colors.black,
                         borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(12),
-                            bottomRight: Radius.circular(12))
+                          topLeft: Radius.circular(12),
+                          bottomRight: Radius.circular(12),
+                        ),
                       ),
-                      child: const Icon(Icons.add,color: Colors.white),
+                      child: const Icon(
+                        Icons.add,
+                        color: Colors.white,
+                      ),
                     ),
-                  )
+                  ),
                 ],
               ),
             ),
--- lib/pages/item_page.dart
+++ lib/pages/item_page.dart
@@ -7,217 +7,154 @@ class ItemPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Shoe shoe = ModalRoute.of(context)?.settings.arguments as Shoe;
+
+    // Get screen width and height
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
       appBar: AppBar(),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          //image
-         SizedBox(
-            height: 400,
-            child: Center(child: Image.asset(shoe.imagePath)),
-          ),
-          //name of product
-          Container(
-              margin:  const EdgeInsets.only(left: 50),
-              child: Text(shoe.name,
-                style: const TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 28,
-              ),
-              ),
-          ),
-          //colors option
+      body: SingleChildScrollView( // Ensure scrolling on small screens
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Image
+            SizedBox(
+              height: screenHeight * 0.4, // Image takes 40% of screen height
+              child: Center(child: Image.asset(shoe.imagePath, fit: BoxFit.contain)),
+            ),
 
-          Column(
-            children: [
-              Container(
-                margin:  const EdgeInsets.only(left: 50),
-                padding: EdgeInsets.only(top: 24),
-                child: const Text("Colors",
-                  style: TextStyle(
-                    fontWeight: FontWeight.w600,
-                    fontSize: 16,
-                  ),
+            // Name of product
+            Container(
+              margin: EdgeInsets.only(left: screenWidth * 0.1), // 10% from left
+              child: Text(
+                shoe.name,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: screenWidth * 0.08, // Font size based on screen width
                 ),
               ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Container(
-                margin:  EdgeInsets.only(left: 50,top: 12),
-                height: 35,
-                width: 35,
-               decoration: BoxDecoration(
-                 color: Colors.grey[400],
-                 borderRadius: BorderRadius.circular(24),
-               ),
-              ),
-              Container(
-                margin:  EdgeInsets.only(left: 20,top: 12),
-                height: 35,
-                width: 35,
-                decoration: BoxDecoration(
-                  color: Colors.black,
-                  borderRadius: BorderRadius.circular(24),
+            ),
+
+            // Colors option header
+            Padding(
+              padding: EdgeInsets.only(left: screenWidth * 0.1, top: 24),
+              child: const Text(
+                "Colors",
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 16,
                 ),
               ),
-              Container(
-                margin:  EdgeInsets.only(left: 20,top: 12),
-                height: 35,
-                width: 35,
-                decoration: BoxDecoration(
-                  color: Colors.blueGrey,
-                  borderRadius: BorderRadius.circular(24),
-                ),
-              ),
-              Container(
-                margin:  EdgeInsets.only(left: 20,top: 12),
-                height: 35,
-                width: 35,
-                decoration: BoxDecoration(
-                  color: Colors.brown,
-                  borderRadius: BorderRadius.circular(24),
-                ),
-              ),
-            ],
-          ),
-          //size
-          Container(
-            margin: EdgeInsets.only(top: 24,left: 50),
-            child: const Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            ),
+
+            // Color options (row of color circles)
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                Text("Size",
-                  style: TextStyle(
+                _colorOption(Colors.grey[400]),
+                _colorOption(Colors.black),
+                _colorOption(Colors.blueGrey),
+                _colorOption(Colors.brown),
+              ],
+            ),
+
+            // Size options header
+            Padding(
+              padding: EdgeInsets.only(left: screenWidth * 0.1, top: 24),
+              child: const Text(
+                "Size",
+                style: TextStyle(
                   fontWeight: FontWeight.w500,
                   fontSize: 16,
                 ),
-                ),
-                Row(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.only(right: 12),
-                      child: Text("EU",
-                        style: TextStyle(
-                          fontWeight: FontWeight.w500,
-                          fontSize: 16,
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(right: 12),
-                      child: Text("IND",
-                        style: TextStyle(
-                          fontWeight: FontWeight.w500,
-                          fontSize: 16,
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(right: 60),
-                      child: Text("US",
-                        style: TextStyle(
-                          fontWeight: FontWeight.w500,
-                          fontSize: 16,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
+              ),
             ),
-          ),
-          Container(
-            child: Row(
+
+            // Size selection options
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                Container(
-                  margin: EdgeInsets.only(left: 50,top: 8),
-                  height: 45,
-                  width: 45,
-                  decoration: BoxDecoration(
-                      color: Colors.grey[300],
-                      borderRadius: BorderRadius.circular(12)
-                  ),
-                  child: Center(child: Text('7.5',style: TextStyle(fontWeight: FontWeight.w600),)),
-                ),
-                Container(
-                  margin: EdgeInsets.only(left: 24,top: 8),
-                  height: 45,
-                  width: 45,
-                  decoration: BoxDecoration(
-                      color: Colors.grey[300],
-                      borderRadius: BorderRadius.circular(12)
-                  ),
-                  child: Center(child: Text('8',style: TextStyle(fontWeight: FontWeight.w600),)),
-                ),
-                Container(
-                  margin: EdgeInsets.only(left: 24,top: 8),
-                  height: 45,
-                  width: 45,
-                  decoration: BoxDecoration(
-                      color: Colors.grey[300],
-                      borderRadius: BorderRadius.circular(12)
-                  ),
-                  child: Center(child: Text('8.5',style: TextStyle(fontWeight: FontWeight.w600),)),
-                ),
-                Container(
-                  margin: EdgeInsets.only(left: 24,top: 8),
-                  height: 45,
-                  width: 45,
-                  decoration: BoxDecoration(
-                      color: Colors.grey[300],
-                      borderRadius: BorderRadius.circular(12)
-                  ),
-                  child: Center(child: Text('9',style: TextStyle(fontWeight: FontWeight.w600),)),
-                ),
-                Container(
-                  margin: EdgeInsets.only(left: 24,top: 8),
-                  height: 45,
-                  width: 45,
-                  decoration: BoxDecoration(
-                      color: Colors.grey[300],
-                      borderRadius: BorderRadius.circular(12)
-                  ),
-                  child: Center(child: Text('9.5',style: TextStyle(fontWeight: FontWeight.w600),)),
-                ),
+                _sizeOption("7.5"),
+                _sizeOption("8"),
+                _sizeOption("8.5"),
+                _sizeOption("9"),
+                _sizeOption("9.5"),
               ],
             ),
-          ),
-          // price and buy buttom
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                margin:  const EdgeInsets.only(left: 50,top: 32),
-                padding: EdgeInsets.only(top: 24),
-                child: Text('\$'+shoe.price,
-                  style: TextStyle(
-                    fontWeight: FontWeight.w600,
-                    fontSize: 32,
+
+            // Price and buy button
+            Padding(
+              padding: EdgeInsets.only(left: screenWidth * 0.1, top: 32),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    '\$' + shoe.price,
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: screenWidth * 0.08, // Font size based on screen width
+                    ),
                   ),
-                ),
-              ),
-              Container(
-                margin:  const EdgeInsets.only(right: 50,top: 56),
-                padding: EdgeInsets.only(right:20,top: 12, bottom: 12,left: 20),
-                decoration: BoxDecoration(
-                  color: Colors.black,
-                  borderRadius: BorderRadius.circular(32)
-                ),
-                child: Text("Buy Now",
-                  style: TextStyle(
-                    fontWeight: FontWeight.w600,
-                    fontSize: 24,
-                    color: Colors.white
+                  GestureDetector(
+                    onTap: () {
+                      // Implement your buy action here
+                    },
+                    child: Container(
+                      padding: EdgeInsets.symmetric(vertical: 12, horizontal: 24),
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(32),
+                      ),
+                      child: const Text(
+                        "Buy Now",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 24,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
                   ),
-                ),
+                ],
               ),
-            ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // Helper widget for size options
+  Widget _sizeOption(String size) {
+    return Container(
+      margin: EdgeInsets.only(left: 24, top: 8),
+      height: 45,
+      width: 45,
+      decoration: BoxDecoration(
+        color: Colors.grey[300],
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Center(
+        child: Text(
+          size,
+          style: TextStyle(
+            fontWeight: FontWeight.w600,
           ),
-        ],
+        ),
+      ),
+    );
+  }
+
+  // Helper widget for color options
+  Widget _colorOption(Color? color) {
+    return Container(
+      margin: EdgeInsets.only(left: 50, top: 12),
+      height: 35,
+      width: 35,
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(24),
       ),
     );
   }
